73ca8b01db5f76fc6b11d4d6b77bbfdb21a91fd4
Faizan <f24aalam@gmail.com>
2021-07-30 15:42:41 +0530

    Livewire component for  tasks lists

---
+++ resources/views/livewire/tasks/tasks-list.blade.php
@@ -0,0 +1,46 @@
+<div>
+    <ul>
+        <li class="py-2 px-4">
+            <div class="flex items-start justify-start">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+                </svg>
+                <div class="flex-1 leading-6">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus maxime numquam vitae sequi illo necessitatibus enim quam aliquam eligendi quis modi vel doloribus, inventore debitis suscipit, recusandae in dolores nemo.
+                </div>
+            </div>
+            <div class="flex justify-end items-end gap-2 border-b pb-2">
+                <button class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition">Delete</button>
+                <button class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring focus:ring-green-300 disabled:opacity-25 transition">Mark As Completed</button>
+            </div>
+        </li>
+        <li class="py-2 px-4">
+            <div class="flex items-start justify-start">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+                </svg>
+                <div class="flex-1 leading-6">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit vel in officia. Perspiciatis, maxime est impedit odit quis architecto tempora adipisci in ipsam autem at officiis laboriosam voluptatibus exercitationem doloribus!
+                </div>
+            </div>
+            <div class="flex justify-end items-end gap-2 border-b pb-2">
+                <button class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition">Delete</button>
+                <button class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring focus:ring-green-300 disabled:opacity-25 transition">Mark As Completed</button>
+            </div>
+        </li>
+        <li class="py-2 px-4">
+            <div class="flex items-start justify-start">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+                </svg>
+                <div class="flex-1 leading-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quidem mollitia quaerat cumque esse id pariatur corrupti iste, libero minus quasi dolorum voluptatum velit. Odio eius excepturi magnam similique numquam.
+                </div>
+            </div>
+            <div class="flex justify-end items-end gap-2 border-b pb-2">
+                <button class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition">Delete</button>
+                <button class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring focus:ring-green-300 disabled:opacity-25 transition">Mark As Completed</button>
+            </div>
+        </li>
+    </ul>
+</div>
