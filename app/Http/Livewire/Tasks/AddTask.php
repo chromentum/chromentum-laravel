f1552871275f5a42f3ca24d7baa4ef0e0d230784
Faizan <f24aalam@gmail.com>
2021-08-02 15:55:34 +0530

    Added Method to store task

--- app/Http/Livewire/Tasks/AddTask.php
+++ app/Http/Livewire/Tasks/AddTask.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Livewire\Tasks;
 
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class AddTask extends Component
 {
+    public $description;
+
+    protected $rules = [
+        'description' => 'required|max:140',
+    ];
+
     public function render()
     {
         return view('livewire.tasks.add-task');
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        $task = new Task();
+        $task->user_id = Auth::user()->id;
+        $task->description = $this->description;
+        if ( $task->save() ) {
+            $this->description = null;
+        }
     }
 }
--- resources/views/livewire/tasks/add-task.blade.php
+++ resources/views/livewire/tasks/add-task.blade.php
@@ -1,10 +1,9 @@
 <div>
-    <div>
-        <form class="flex relative">
-            <x-jet-input id="text" class="mt-1 flex-1" type="text" name="text" :value="old('email')" placeholder="Enter task and hit enter" required autofocus />
-            <x-jet-button class="hidden">
-                {{ __('Add Task') }}
-            </x-jet-button>
-        </form>
-    </div>
+    <form class="flex flex-col relative" wire:submit.prevent="submit">
+        <x-jet-input wire:model.defer="description" id="text" class="mt-1 flex-1" type="text" name="text" :value="old('email')" placeholder="Enter task and hit enter" autofocus required/>
+        <x-jet-button class="hidden">
+            {{ __('Add Task') }}
+        </x-jet-button>
+        <x-jet-input-error for="description" class="mt-2" />
+    </form>
 </div>
