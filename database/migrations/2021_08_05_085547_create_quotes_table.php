5034648adf9c6117e2f41f415b8bb3ca96dbe77f
Faizan <f24aalam@gmail.com>
2021-08-05 14:29:35 +0530

    Added Model And Migrations

---
+++ database/migrations/2021_08_05_085547_create_quotes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->string('quote');
+            $table->string('author');
+            $table->string('tags')->nullable();
+            $table->enum('scope', ['global', 'local']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
