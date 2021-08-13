05727bbcaa74544bdc2f35d492effc7b84de524a
Faizan <f24aalam@gmail.com>
2021-08-13 17:04:51 +0530

    Added CTA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,7 +94,8 @@
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
             <div class="text-center mb-20">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">New Tab Extension For Chrome Browser
+                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">New Tab Extension For Chrome
+                    Browser
                 </h1>
                 <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Blue bottle crucifix vinyl
                     post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing
@@ -155,6 +156,40 @@
             </div>
             <button
                 class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Download</button>
+        </div>
+    </section>
+
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex items-center md:flex-row flex-col">
+            <div class="flex flex-col md:pr-10 md:mb-0 mb-6 pr-0 w-full md:w-auto md:text-left text-center">
+                <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Chromentum</h2>
+                <h1 class="md:text-3xl text-2xl font-medium title-font text-gray-900">Get it right now
+                </h1>
+            </div>
+            <div class="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
+                <button
+                    class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                    <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" class="w-10 h-10">
+                        <path
+                            d="M 15 3 C 11.074 3 7.5981563 4.8945469 5.4101562 7.8105469 L 9.7519531 12.152344 C 10.767953 10.283344 12.724 9 15 9 C 16.342 9 17.568359 9.4552188 18.568359 10.199219 L 25.800781 10.199219 L 20.369141 12.371094 C 20.760141 13.168094 21 14.053 21 15 C 21 16.972 20.035359 18.707781 18.568359 19.800781 L 18.599609 19.800781 L 13.839844 26.941406 C 14.220844 26.978406 14.608 27 15 27 C 21.628 27 27 21.628 27 15 C 27 8.372 21.628 3 15 3 z M 5.40625 7.8125 C 3.90225 9.8175 3 12.3 3 15 C 3 21.229 7.7473125 26.346453 13.820312 26.939453 L 15.828125 20.916016 C 15.554125 20.954016 15.283 21 15 21 C 11.687 21 9 18.313 9 15 L 5.40625 7.8125 z M 15 11 A 4 4 0 0 0 11 15 A 4 4 0 0 0 15 19 A 4 4 0 0 0 19 15 A 4 4 0 0 0 15 11 z" />
+                    </svg>
+                    <span class="ml-4 flex items-start flex-col leading-none">
+                        <span class="text-xs text-gray-600 mb-1">GET IT ON</span>
+                        <span class="title-font font-medium">Chrome Store</span>
+                    </span>
+                </button>
+                <button
+                    class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                    <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-10 h-10">
+                        <path
+                            d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z" />
+                    </svg>
+                    <span class="ml-4 flex items-start flex-col leading-none">
+                        <span class="text-xs text-gray-600 mb-1">FORK IT ON</span>
+                        <span class="title-font font-medium">Github</span>
+                    </span>
+                </button>
+            </div>
         </div>
     </section>
 
